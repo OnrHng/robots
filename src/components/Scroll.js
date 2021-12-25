84ef393775edcec9702358fbbcedd8cfb219575d
Onur Hangül <42890353+OnrHng@users.noreply.github.com>
2021-12-25 18:59:09 +0100

    Update Scroll.js

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,7 @@ import React ,{Component} from 'react';
 class Scroll extends Component {
 	render(){
 		return (
-		  <div style={{overflowY:'scroll', border:'1px solid blue', height:'800px'}}>
+		  <div style={{overflowY:'scroll', border:'1px solid blue', height:'1200px'}}>
 		  	{this.props.children}
 		  </div>
 			);
